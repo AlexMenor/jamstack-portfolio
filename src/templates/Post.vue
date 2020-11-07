3bfcd72e11b0befe5efd97c6af023342b5f317e3
Alex Menor <alex4menor@gmail.com>
2020-11-07 19:16:18 +0100

    adds post title in meta tag

--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -2,6 +2,16 @@
   <h1>{{ $page.post.title }}</h1>
 </template>
 
+<script>
+export default {
+  metaInfo() {
+    return {
+      title: this.$page.post.title,
+    };
+  },
+};
+</script>
+
 <page-query>
 query ($id: ID!) {
   post(id: $id) {
