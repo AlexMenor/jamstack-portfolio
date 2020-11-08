0a8b81fe4194856a48d9fb95d6dee5ad8e1f04b1
Alex Menor <alex4menor@gmail.com>
2020-11-08 19:05:58 +0100

    styling post preview

--- src/components/PostPreview.vue
+++ src/components/PostPreview.vue
@@ -1,7 +1,14 @@
 <template>
-  <g-link :to="post.slug" style="display: block; text-decoration: none">
-    <h1>{{ post.title }}</h1>
-    <p>{{ post.description }}</p>
+  <g-link
+    :to="post.slug"
+    style="display: block; text-decoration: none"
+    id="root"
+  >
+    <div class="card">
+      <h1>{{ post.title }}</h1>
+      <img :src="post.picture" />
+      <p>{{ post.description }}</p>
+    </div>
   </g-link>
 </template>
 
@@ -10,3 +17,30 @@ export default {
   props: ["post"],
 };
 </script>
+
+<style scoped>
+#root {
+  max-width: 800px;
+}
+
+img {
+  max-width: 100%;
+}
+
+.card {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+  padding-top: 2em;
+  padding-bottom: 2em;
+  padding-left: 4em;
+  padding-right: 4em;
+  margin-bottom: 2em;
+}
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
