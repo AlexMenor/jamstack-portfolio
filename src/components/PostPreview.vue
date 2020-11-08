dfdd6262fdf083cc9975446ec19fb5340ff63882
Alex Menor <alex4menor@gmail.com>
2020-11-08 19:24:15 +0100

    responsiveness of my latest posts

--- src/components/PostPreview.vue
+++ src/components/PostPreview.vue
@@ -5,7 +5,7 @@
     id="root"
   >
     <div class="card">
-      <h1>{{ post.title }}</h1>
+      <p id="title">{{ post.title }}</p>
       <img :src="post.picture" />
       <p>{{ post.description }}</p>
     </div>
@@ -34,11 +34,32 @@ img {
   align-items: center;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
   transition: 0.3s;
-  padding-top: 2em;
-  padding-bottom: 2em;
-  padding-left: 4em;
-  padding-right: 4em;
-  margin-bottom: 2em;
+  padding: 1em;
+  margin-bottom: 1em;
+}
+
+#title {
+  font-weight: bold;
+  font-size: 1rem;
+}
+p {
+  font-size: 0.8rem;
+}
+
+@media (min-width: 600px) {
+  .card {
+    padding-top: 2em;
+    padding-bottom: 2em;
+    padding-left: 4em;
+    padding-right: 4em;
+    margin-bottom: 2em;
+  }
+  #title {
+    font-size: 2rem;
+  }
+  p {
+    font-size: 1rem;
+  }
 }
 .card:hover {
   box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
