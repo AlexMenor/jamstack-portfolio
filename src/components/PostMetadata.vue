8648e4f958ac48f6e511b88077948f91d52be4bc
Alex Menor <alex4menor@gmail.com>
2020-11-10 20:05:50 +0100

    adds post metadata

---
+++ src/components/PostMetadata.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="post-metadata-root">
+    <p>{{ createdAtFormatted }}</p>
+    <tags :tags="tags"></tags>
+  </div>
+</template>
+
+<script>
+import Tags from "~/components/Tags.vue";
+export default {
+  props: ["tags", "createdAt"],
+  components: { Tags },
+  computed: {
+    createdAtFormatted() {
+      return new Intl.DateTimeFormat("en-US", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      }).format(new Date(this.createdAt));
+    },
+  },
+};
+</script>
+
+<style scoped>
+#post-metadata-root {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+</style>
---
+++ src/components/Tags.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="tags-root">
+    <div class="tag" v-for="(tag, i) in tags" :key="i">{{ tag }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["tags"],
+};
+</script>
+
+<style>
+#tags-root {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+.tag {
+  margin-right: 10px;
+  padding: 5px;
+  border: 1px solid grey;
+  font-size: 0.8rem;
+}
+</style>
